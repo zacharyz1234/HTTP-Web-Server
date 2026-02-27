c785fffacbf354cca33a856826d5541033334451
Zachary Zeldin <47404563+zacharyz1234@users.noreply.github.com>
2026-02-27 18:33:00 -0500

    Added comments to the server side because I forgot to while coding it originally

--- Zeldin_WebServer.py
+++ Zeldin_WebServer.py
@@ -16,16 +16,26 @@ while True:
 
     try:
         reqFileName = connection_socket.recv(1024).decode()
+
+        # Partitions the file by replacing all / with empty characters and then 
+        # splits the string anywhere there is a space, therefore separating the file
+        # name from everything else
         fileName = reqFileName.replace("/", "")
         fileName = fileName.split(" ")
+
+        # Reads the file name array's second element because that is where the file
+        # name would be after the partitioning
         with open(fileName[1], "r") as file:
+            # Sends the 200 OK message
             connection_socket.send("HTTP/1.0 200 OK".encode())
+
+            # Reads the data and sends it to the client to bee printed out
             data = file.read(1024)
             while data:
                 connection_socket.sendall(data.encode())
                 data = file.read(1024)
 
-
+    # If file isn't found it will send the error 404 message
     except FileNotFoundError:
         error = "HTTP/1.0 404 Not Found"
         connection_socket.send(error.encode())
